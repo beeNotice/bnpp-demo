8238c4263ba0e1ed2e043dd0dc2bf0dc36e168cc
Fabien MARTIN <fmartin>
2022-07-12 10:58:51 +0200

    first commit

---
+++ src/main/java/com/tanzu/bnpp/BnppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tanzu.bnpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnppDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanzu/bnpp/BnppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanzu.bnpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
